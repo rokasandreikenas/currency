f2f9970ff0f0c9b7dddd7cd1c4774890a9450c05
rokasandreikenas <35290383+rokasandreikenas@users.noreply.github.com>
2019-11-29 15:20:03 +0200

    Updates

---
+++ precache-manifest.094df63753fc29aeabb26714d7f7ecb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac77e5e1139769b3623b81ef2f8e3675",
+    "url": "/currency/index.html"
+  },
+  {
+    "revision": "c489065fe28deb5da434",
+    "url": "/currency/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "ced0e2243631df329ad8",
+    "url": "/currency/static/css/main.f5fc13fe.chunk.css"
+  },
+  {
+    "revision": "c489065fe28deb5da434",
+    "url": "/currency/static/js/2.ac1f468d.chunk.js"
+  },
+  {
+    "revision": "ced0e2243631df329ad8",
+    "url": "/currency/static/js/main.c71f3cd4.chunk.js"
+  },
+  {
+    "revision": "b7e8dad65c7ccab2d3dc",
+    "url": "/currency/static/js/runtime-main.f4fe4752.js"
+  }
+]);
